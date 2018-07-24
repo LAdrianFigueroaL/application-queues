d61681fb1a4652cd0c4755a568910505a2abe5c6
Adrian Figueroa <neo.adrian@gmail.com>
2018-07-24 13:49:02 -0500

    App de Colas

---
+++ server/sockets/socket.js
@@ -0,0 +1,53 @@
+const { io } = require('../server');
+const { TicketControl } = require('../classes/ticket-control');
+
+
+let ticketControl = new TicketControl();
+
+
+io.on('connection', (client) => {
+
+    client.on('siguienteTicket', (data, callback) => {
+
+        let siguiente = ticketControl.siguienteTicket();
+        console.log(siguiente);
+        callback(siguiente);
+    });
+
+    client.emit('estadoActual', {
+        actual: ticketControl.getUltimoTicket(),
+        ultimos4: ticketControl.getUltimos4()
+    })
+
+    client.on('atenderTicket', (data, callback) => {
+        if (!data.escritorio) {
+            return callback({
+                err: true,
+                mensaje: 'El escritorio es necesario'
+            })
+        }
+
+
+        let atenderTicket = ticketControl.atenderTicket(data.escritorio);
+
+        callback(atenderTicket);
+
+        //actualizar pantalla
+        //console.log(atenderTicket);
+        if (atenderTicket === 'No hay tickets') {
+            client.broadcast.emit('ultimos4', {
+                actual: ticketControl.getUltimoTicket(),
+                ultimos4: ticketControl.getUltimos4(),
+                sonido: false
+            });
+        } else {
+            client.broadcast.emit('ultimos4', {
+                actual: ticketControl.getUltimoTicket(),
+                ultimos4: ticketControl.getUltimos4(),
+                sonido: true
+            });
+        }
+
+    })
+
+});
